2b1f31e232920c6dca448a398b242d301bba5822
Dan Brown <slightlyoffbeat@gmail.com>
2015-02-18 17:04:59 -0800

    adding 'push-to-play' gif short code

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -11,6 +11,7 @@ Custom Shortcodes for BitTorrent Blog
 /*****************
 CUSTOM SHORT CODES
 *****************/
+// button shortcode
 function bt_button( $atts, $content = null ) {
   extract(shortcode_atts(array(
       'centered' => 'yes',
@@ -33,8 +34,30 @@ function bt_button( $atts, $content = null ) {
   return '<div class="button-wrap ' . $the_center . '"><a href="' . $link . '" target="_blank"><button type="button" class="btn btn-bt ' . $the_size . ' ' . $tracking . '">' . $content . '</button></a></div>';
 }
 
+function bt_gif( $atts, $content = null ) {
+  extract(shortcode_atts(array(
+      'static' => '',
+      'gif' => '',
+      'align' => 'alignnone',
+    ), $atts));
+  $the_gif = "'" . $gif . "'";
+  $the_align = $align;
+  if ($align == "center") {
+    $the_align = "aligncenter";
+  }
+  elseif ($align == "left") {
+    $the_align = "alignleft";
+  }
+  elseif ($align == "right") {
+    $the_align = "alignright";
+  }
+
+  return '<img class="' . $the_align . '" src="' . $static . '" onclick="this.src=' . $the_gif . '">';
+}
+
 function register_shortcodes(){
    add_shortcode('button', 'bt_button');
+   add_shortcode('clickgif', 'bt_gif');
 }
 
 add_action( 'init', 'register_shortcodes');
